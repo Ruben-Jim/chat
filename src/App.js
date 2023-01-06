327f5469850e5f9241612e60e43b1e8c27af9813
Ruben <ruben.jim.co@gmail.com>
2023-01-05 19:17:27 -0800

    main commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,38 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Chat from './components/Chat';
-import {Helmet} from "react-helmet";
-
 import { auth } from './firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import {Helmet} from "react-helmet";
 
 const style = {
   appContainer: `max-w-[728px] mx-auto text-center`,
   sectionContainer: `flex flex-col h-[90vh] bg-gray-100 mt-10 shadow-xl border relative`,
 };
 
+
+
 function App() {
   const [user] = useAuthState(auth);
   //  console.log(user)
   return (
+    
     <div className={style.appContainer}>
       <section className='{style.sectionContainer}'>
-      
-        {/* Navbar */}
-        <Navbar />
-        {user ? <Chat /> : null}
         <Helmet>
           <meta charSet="utf-8"/>
           <title>My Title</title>
           <link rel="canonical" href="http://mysite.com/example"/>
-          <meta name="description" content="Testing icon and title" />
         </Helmet>
+      
+        {/* Navbar */}
+        <Navbar />
+        {user ? <Chat /> : null}
+        
       </section>
     </div>
   );
 }
+
 
 export default App;
